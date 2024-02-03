da01acd186c3c3490ab252a2e967702a79664d72
shruti garg <hellocoders8213@gmail.com>
2024-02-03 16:01:40 +0530

    styling done

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,44 +1,10 @@
-/* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
-
-const ShowMoreBtn = (props) => {
-	const { api, setPage, imgLength, loading } = props;
-	return (
-		<div
-			style={{
-				display: "flex",
-				justifyContent: "center",
-				paddingBottom: "20px",
-			}}
-		>
-			<button
-				className={imgLength > 0 ? "show" : "hidden"}
-				onClick={() => {
-					setPage((prevPage) => prevPage + 1);
-					api();
-				}}
-				disabled={loading}
-			>
-				{loading ? "Loading..." : "Show More"}
-			</button>
-		</div>
-	);
-};
+import ShowMoreBtn from "./ShowMoreBtn";
+import ImageComponent from "./ImageComponent";
+import SearchForm from "./SearchForm";
 
 const Body = () => {
-	const handleMediaChange = (x) => {
-		setPerPage(x.matches ? 6 : 12);
-	};
-
-	useEffect(() => {
-		const x = window.matchMedia("(max-width: 400px)");
-		handleMediaChange(x); // Initial check
-		x.addEventListener("change", handleMediaChange); // Listen for changes
-
-		return () => {
-			x.removeEventListener("change", handleMediaChange); // Cleanup event listener
-		};
-	}, []); // Empty dependency array ensures the effect runs only once on mount
+	//State Variables defined
 	const accessKey = "WCO1Nkc4C26M4kW-Oi6JOlTCYpPVQG1BTAkKrgN9tKM";
 	const [keyword, setKeyword] = useState(" ");
 	const [image, setImage] = useState([]);
@@ -46,24 +12,48 @@ const Body = () => {
 	const [page, setPage] = useState(1);
 	const [perPage, setPerPage] = useState(12);
 
+	//On submit of form , this function is called
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setPage(1);
+		setImage([]);
+		imageApiData();
+	};
+
+	//This function is called when media chnages for small devices load 6 images otherwise 12 images
+	const handleMediaChange = (x) => {
+		setPerPage(x.matches ? 6 : 12);
+	};
+
+	//This useEffect hook is used to see the change of media
+	useEffect(() => {
+		const x = window.matchMedia("(max-width: 400px)");
+		handleMediaChange(x);
+		x.addEventListener("change", handleMediaChange);
+
+		return () => {
+			x.removeEventListener("change", handleMediaChange);
+		};
+	}, []);
+
+	//This useEffect hook is used to setImage as empty array and setPage as 1 whenever keyword and perPage value changes
 	useEffect(() => {
 		setImage([]);
 		setPage(1);
-		// imageApiData();
 	}, [keyword, perPage]);
+
+	//Async function fetching data from API server
 	const imageApiData = async () => {
 		try {
 			setLoading(true);
 			const data = await fetch(
-				`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=${perPage}`
+				`https://api.unsplash.com/search/photos?page=${page}&query=${keyword.trim()}&client_id=${accessKey}&per_page=${perPage}`
 			);
 			const json = await data.json();
 			if (page === 1) {
-				// On the first load, display only the first batch of images
-				setImage(json.results.slice(0, perPage));
+				setImage(json.results.slice(0, perPage)); //perPage = 12
 			} else {
-				// On subsequent loads (Show More), concatenate new images
-				setImage((prevImages) => [...prevImages, ...json.results]);
+				setImage((prevImages) => [...prevImages, ...json.results]); // add previous images with the new loaded images
 			}
 		} catch (error) {
 			console.error("Error fetching images:", error);
@@ -74,42 +64,23 @@ const Body = () => {
 	};
 
 	return (
+		//Main body div
 		<div className="body">
-			<form
-				onSubmit={(e) => {
-					e.preventDefault();
-					setPage(1);
-					setImage([]);
-					imageApiData();
-				}}
-			>
-				{" "}
-				<input
-					type="text"
-					placeholder="Search Images"
-					onChange={(e) => setKeyword(e.target.value)}
-				/>
-				<button onClick={imageApiData} type="submit">
-					Search
-				</button>
-			</form>
-
+			{/* SearchForm component  */}
+			<SearchForm
+				handleSubmit={handleSubmit}
+				setKeyword={setKeyword}
+				api={imageApiData}
+			/>
+			{/* 
+			for no image load show text Loading Images..... but as soon as image loads put this into this ImageComponent as give this image array as props  */}
 			{image.length > 0 ? (
-				<div className="image-result">
-					{image.map((img, index) => (
-						<a
-							key={index}
-							href={img.links.html}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<img src={img.urls.small} alt={`Unsplash Image ${index}`} />
-						</a>
-					))}
-				</div>
+				<ImageComponent image={image} />
 			) : (
-				<p>Loading Images</p>
+				<p>Loading Images..........</p>
 			)}
+
+			{/* show more button is used to show another 12 images below the existing ones on click of this button  */}
 			<ShowMoreBtn
 				imgLength={image.length}
 				api={imageApiData}
